44f23c98b48ad252f2a5b38cd4f33d69e7bff58f
tronixs <92642123+tronixs@users.noreply.github.com>
2023-02-18 11:02:34 +0300

    66666

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,65 +4,71 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\NewsStatus;
 use App\Http\Controllers\Controller;
 use App\Models\News;
+use App\QueryBuilders\CategoriesQueryBuilder;
+use App\QueryBuilders\NewsQueryBuilder;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class NewsController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param NewsQueryBuilder $newsQueryBuilder
      * @return View
      */
-    public function index(): View
+    public function index(NewsQueryBuilder $newsQueryBuilder): View
     {
-        $model = new News();
-        $newsList = $model->getNews();
-        $join = \DB::table('news')
-            ->join('category_has_news as chn', 'news.id', '=', 'chn.news_id')
-            ->leftJoin('categories', 'chn.category_id', '=', 'categories.id')
-            ->select("news.*", 'chn.category_id', 'categories.title as ctitle')
-            ->get();
-
-
-//dd($join, $where, $where_new);
         return \view('admin.news.index', [
-            'newsList' => $newsList,
+            'newsList' => $newsQueryBuilder->getNewsWithPagination(),
         ]);
     }
 
     /**
      * Show the form for creating a new resource.
      *
+     * @param CategoriesQueryBuilder $categoriesQueryBuilder
      * @return View
      */
-    public function create(): View
+    public function create(CategoriesQueryBuilder $categoriesQueryBuilder,): View
     {
-        return \view('admin.news.create');
+        return \view('admin.news.create', [
+            'categories' => $categoriesQueryBuilder->getAll(),
+            'statuses' => NewsStatus::all(),
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         $request->validate([
             'title' => 'required',
         ]);
 
-        return response()->json($request->only(['title', 'author', 'description']));
+        $news = new News($request->except('_token', 'category_id'));
+
+        if ($news->save()) {
+            return \redirect()->route('admin.news.index')->with('success', 'news add successfully');
+        }
+
+        return \back()->with('error', 'failure to add news');
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show($id)
     {
@@ -72,31 +78,42 @@ class NewsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param News $news
+     * @param CategoriesQueryBuilder $categoriesQueryBuilder
+     * @return View
      */
-    public function edit($id)
+    public function edit(News $news, CategoriesQueryBuilder $categoriesQueryBuilder): View
     {
-        //
+        return \view('admin.news.edit', [
+            'news' => $news,
+            'categories' => $categoriesQueryBuilder->getAll(),
+            'statuses' => NewsStatus::all(),
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param News $news
+     * @return RedirectResponse
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, News $news): RedirectResponse
     {
-        //
+        $news = $news->fill($request->except('_token', 'category_id'));
+        if ($news->save()) {
+            $news->categories()->sync((array) $request->input('category_ids'));
+            return \redirect()->route('admin.news.index')->with('success', 'news add successfully');
+        }
+
+        return \back()->with('error', 'failure to add news');
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
